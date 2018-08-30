1ecad951fc2b1a4cea820e401e70b959ce7eff28
jiangxiaosong <1273398724@qq.com>
2018-08-30 19:38:33 +0800

    init1

--- middleware/loginStatusCheck.js
+++ middleware/loginStatusCheck.js
@@ -9,7 +9,7 @@ exports.checkLogin = function (req, res, next) {
 exports.checkNotLogin = function (req, res, next) {
   if (req.session.user) {
     req.flash('error', '已登录!');
-    return res.redirect('/'); //返回主页
+    return res.redirect('/'); //返回主页 
   }
   next();
 }
